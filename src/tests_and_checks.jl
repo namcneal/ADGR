c9d8af61cfd94feae0345635a8e3f03574ff9bfb
Noah <namcneal@umich.edu>
2023-03-25 10:17:09 -0400

    Leveraging einsum more

--- src/tests_and_checks.jl
+++ src/tests_and_checks.jl
@@ -62,14 +62,14 @@ function test_riemannian_symmetry(Riem::Array{Float64}, point::Vector)
             should_be_zero  = (Riem[i,j,k,l] + Riem[i,j,l,k])^2            
             @assert should_be_zero < 1e-12 "Failed asymmetry on indices [$(i), $(j), $(k), $(l)]"
         catch AssertionError
-            @show Riem[i,j,k,l] + Riem[i,j,l,k]
+            @show (Riem[i,j,k,l] + Riem[i,j,l,k])^2
         end 
 
         try 
-            should_be_zero += (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])^2
+            should_be_zero = (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])^2
             @assert should_be_zero < 1e-12 "Failed asymmetry on indices [$(i), $(j), $(k), $(l)]"
         catch
-            @show (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])
+            @show (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])^2
         end
 
     end
