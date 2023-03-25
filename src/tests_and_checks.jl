456bc4aa5033df0ac13f6cc827c92a485e86794e
Noah <namcneal@umich.edu>
2023-03-25 10:12:09 -0400

    Leveraging einsum more

--- src/tests_and_checks.jl
+++ src/tests_and_checks.jl
@@ -56,11 +56,22 @@ function test_riemannian_symmetry(Riem::Array{Float64}, point::Vector)
 
     d = length(point)
 
-    for (i,j,k,l) in collect(Iterators.product(1:d,1:d,1:d,1:d))
-        should_be_zero  = (Riem[i,j,k,l] + Riem[j,i,l,k])^2
-        should_be_zero += (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])^2
+    for (i,j,k,l) in collect(Iterators.product(1:d,1:d,1:d,1:d)) 
 
-        @assert should_be_zero < 1e-12 "Failed asymmetry on indices [$(i), $(j), $(k), $(l)]"
+        try
+            should_be_zero  = (Riem[i,j,k,l] + Riem[j,i,l,k])^2            
+            @assert should_be_zero < 1e-12 "Failed asymmetry on indices [$(i), $(j), $(k), $(l)]"
+        catch AssertionError
+            @show Riem[i,j,k,l] + Riem[j,i,l,k]
+        end 
+
+        try 
+            should_be_zero += (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])^2
+            @assert should_be_zero < 1e-12 "Failed asymmetry on indices [$(i), $(j), $(k), $(l)]"
+        catch
+            @show (Riem[i,j,k,l] + Riem[i,k,l,j] + Riem[i,l,j,k])
+        end
+
     end
 
 end
